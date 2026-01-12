c17c2d95397135721192f4ee500e062623970fe9
khaled <khaledelkhamisy475@gmail.com>
2026-01-12 15:56:18 +0200

    zego service

--- lib/core/constant/constans_keys.dart
+++ lib/core/constant/constans_keys.dart
@@ -1,2 +1,5 @@
 const String kMyProfileImage = 'myProfileImage';
 const String kMyProfileName = 'myProfileName';
+const int kAppId = 2043268088;
+const String kAppSign =
+    '2c9e700053f5ba28ddc9e1d02a19bb728b9d82467bd337483c8a3d8616dcb1dc';
--- lib/features/advisor/add_post/model/category_model.dart
+++ lib/features/advisor/add_post/model/category_model.dart
@@ -2,24 +2,14 @@ class CategoryModel {
   final String id;
   final String name;
   final String type;
-  final DateTime createdAt;
-  final DateTime updatedAt;
 
-  CategoryModel({
-    required this.id,
-    required this.name,
-    required this.type,
-    required this.createdAt,
-    required this.updatedAt,
-  });
+  CategoryModel({required this.id, required this.name, required this.type});
 
   factory CategoryModel.fromJson(Map<String, dynamic> json) {
     return CategoryModel(
       id: json['id'] as String,
       name: json['name'] as String,
       type: json['type'] as String,
-      createdAt: DateTime.parse(json['createdAt']),
-      updatedAt: DateTime.parse(json['updatedAt']),
     );
   }
 }
