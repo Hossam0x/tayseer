d8985ded070281c09461847befb67206101159ce
ATHR <abdulrhmanabukhalifah@gmail.com>
2026-01-21 15:35:30 +0200

    solve auth google and apple and solve home scrol

--- lib/features/shared/home/views/widgets/home_view_body.dart
+++ lib/features/shared/home/views/widgets/home_view_body.dart
@@ -109,7 +109,8 @@ class HomeViewBodyState extends State<HomeViewBody> {
           ]);
         },
         child: CustomScrollView(
-          physics: const AlwaysScrollableScrollPhysics(),
+
+          physics: const ClampingScrollPhysics(),
           cacheExtent: 500.0,
           controller: _scrollController,
           slivers: [
