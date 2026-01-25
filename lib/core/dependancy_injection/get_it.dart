18ba86707af5d260b6748ac658f6adfcaf6d025f
khaledaboelhamd <powershop9640@gmail.com>
2026-01-25 13:02:31 +0200

    fix reels getIt bug

--- lib/core/dependancy_injection/get_it.dart
+++ lib/core/dependancy_injection/get_it.dart
@@ -1,3 +1,4 @@
+import 'package:tayseer/core/models/post_model.dart';
 import 'package:tayseer/features/advisor/add_post/repo/posts_repository.dart';
 import 'package:tayseer/features/advisor/add_post/repo/posts_repository_impl.dart';
 import 'package:tayseer/features/advisor/chat/presentation/manager/chat_messages_cubit_simple.dart';
@@ -6,6 +7,7 @@ import 'package:tayseer/features/advisor/event/repo/event_repo_impl.dart';
 import 'package:tayseer/features/advisor/event_detail/repo/event_detail_repository.dart';
 import 'package:tayseer/features/advisor/event_detail/repo/event_detail_repository_impl.dart';
 import 'package:tayseer/features/advisor/event_detail/view_model/event_detail_cubit.dart';
+import 'package:tayseer/features/advisor/reels/view_model/cubit/reels_cubit.dart';
 import 'package:tayseer/features/advisor/session/data/repos/advisor_session_repo.dart';
 import 'package:tayseer/features/advisor/session/presentation/manager/advisor_session_detailes_cubit.dart';
 import 'package:tayseer/features/advisor/settings/data/repositories/account_management_repository.dart';
@@ -106,6 +108,10 @@ Future<void> setupGetIt() async {
   // Home Cubit
   getIt.registerFactory<HomeCubit>(() => HomeCubit(getIt<HomeRepository>()));
 
+  // Reels cubit
+  getIt.registerFactoryParam<ReelsCubit, PostModel, void>(
+    (post, _) => ReelsCubit(getIt<HomeRepository>(), initialPost: post),
+  );
   // Stories Feature
   getIt.registerLazySingleton<StoriesRepository>(
     () => StoriesRepositoryImpl(getIt<ApiService>()),
@@ -294,16 +300,13 @@ Future<void> setupGetIt() async {
     () => UserProfileEditCubit(getIt<UserProfileRepository>()),
   );
 
-
-
   // Interactions Repository
-getIt.registerLazySingleton<InteractionsRepository>(
-  () => InteractionsRepositoryImpl(getIt<ApiService>()),
-);
+  getIt.registerLazySingleton<InteractionsRepository>(
+    () => InteractionsRepositoryImpl(getIt<ApiService>()),
+  );
 
-// Interactions Cubit
-getIt.registerFactory<InteractionsCubit>(
-  () => InteractionsCubit(getIt<InteractionsRepository>()),
-);
-  
+  // Interactions Cubit
+  getIt.registerFactory<InteractionsCubit>(
+    () => InteractionsCubit(getIt<InteractionsRepository>()),
+  );
 }
