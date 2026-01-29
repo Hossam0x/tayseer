e0310329130ae83b4d9a9ed3fcaf2e46a194b29a
Ahmed-Elshennawy <ahmedlshennawy10@gmail.com>
2026-01-29 16:10:07 +0200

    Add see mirrage profile button

--- lib/features/user/user_profile/views/widgets/user_public_profile_bio.dart
+++ lib/features/user/user_profile/views/widgets/user_public_profile_bio.dart
@@ -107,21 +107,33 @@ class UserPublicProfileBio extends StatelessWidget {
 
           // حالة الجاهزية للزواج
           if (!profile.isMe) ...[
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 6.h),
-              decoration: BoxDecoration(
-                color: profile.availableForMarry
-                    ? AppColors.primary300
-                    : AppColors.secondary400,
-                borderRadius: BorderRadius.circular(34.r),
-              ),
-              child: Text(
-                profile.availableForMarry ? 'متاح الآن' : 'غير متاح',
-                style: Styles.textStyle14.copyWith(
-                  color: AppColors.secondary950,
-                ),
-              ),
-            ),
+            profile.availableForMarry
+                ? Padding(
+                    padding: EdgeInsets.only(top: 10.h),
+                    child: CustomBotton(
+                      width: 200.w,
+                      height: 40.h,
+                      backGroundcolor: AppColors.primary400,
+                      title: 'رؤية ملف الزواج',
+                      onPressed: () {},
+                    ),
+                  )
+                : Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 12.w,
+                      vertical: 6.h,
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.secondary400,
+                      borderRadius: BorderRadius.circular(34.r),
+                    ),
+                    child: Text(
+                      'غير متاح',
+                      style: Styles.textStyle14.copyWith(
+                        color: AppColors.secondary950,
+                      ),
+                    ),
+                  ),
             Gap(12.h),
           ],
 
