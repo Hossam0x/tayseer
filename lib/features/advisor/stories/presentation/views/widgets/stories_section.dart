2e09b3a941ca3fdcf6b37e3413bc9dc73fcc64fc
Ahmed <ahmedlshennawy10@gmail.com>
2026-02-18 11:22:28 +0200

    Finish add stories in i phone

--- lib/features/advisor/stories/presentation/views/widgets/stories_section.dart
+++ lib/features/advisor/stories/presentation/views/widgets/stories_section.dart
@@ -313,7 +313,9 @@ class _AddStoryItem extends StatelessWidget {
                   final camera = await Permission.camera.request();
 
                   if (context.mounted) {
-                    if (photos.isGranted && camera.isGranted) {
+                    final isPhotosGranted =
+                        photos.isGranted || photos.isLimited;
+                    if (isPhotosGranted && camera.isGranted) {
                       final storiesCubit = context.read<StoriesCubit>();
                       Navigator.push(
                         context,
