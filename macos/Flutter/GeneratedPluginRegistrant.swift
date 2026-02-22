aaa88fe5cb6848813bcad24a4a38067999a60002
khaled <khaledelkhamisy475@gmail.com>
2026-02-22 14:08:00 +0200

    Update localization files, plugin registrations, and dependencies
    - Updated Arabic and English localization files to correct and reorder strings.
    - Removed unused imports in GeneratedPluginRegistrant.swift.
    - Updated pubspec.lock to reflect new package versions and their checksums.
    - Adjusted pubspec.yaml to downgrade pro_image_editor dependency version.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,7 +22,6 @@ import geolocator_apple
 import google_sign_in_ios
 import just_audio
 import package_info_plus
-import path_provider_foundation
 import photo_manager
 import pro_video_editor
 import screen_brightness_macos
@@ -54,7 +53,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   ProVideoEditorPlugin.register(with: registry.registrar(forPlugin: "ProVideoEditorPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
