5ae606ae973315a2ea8a77af48452473f050712c
kareemAlfara <kareemalfara@gmailcom>
2026-02-22 15:48:05 +0200

    dd

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,10 +15,18 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// ✅ No afterEvaluate needed
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
