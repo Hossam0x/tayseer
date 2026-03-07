e5d521a361da8bd47cf32698d91fc95b13f4cfe0
khaledaboelhamd <powershop9640@gmail.com>
2026-03-07 11:24:23 +0200

    pass post model to edit post in AddPostView

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -1,6 +1,4 @@
 import 'dart:developer';
-
-import 'package:tayseer/core/enum/add_post_enum.dart';
 import 'package:tayseer/core/enum/male_female.dart';
 import 'package:tayseer/core/enum/user_type.dart';
 import 'package:tayseer/core/models/post_model.dart';
@@ -659,9 +657,8 @@ abstract class AppRouter {
           builder: (_) => BlocProvider(
             create: (context) => AddPostCubit()..getALLCategory(),
             child: AddPostView(
-              addPostEnum: args!['addPostEnum'] as AddPostEnum?,
-              post: args['post'] as PostModel?,
-              isEdit: args['isEdit'] as bool? ?? false,
+              post: args?['post'] as PostModel?,
+              isEdit: args?['isEdit'] as bool? ?? false,
             ),
           ),
         );
--- lib/features/advisor/add_post/view/add_post_view.dart
+++ lib/features/advisor/add_post/view/add_post_view.dart
@@ -1,20 +1,18 @@
-import 'package:tayseer/core/enum/add_post_enum.dart';
+import 'dart:developer';
+
 import 'package:tayseer/core/models/post_model.dart';
 import 'package:tayseer/features/advisor/add_post/view/widget/add_post_body.dart';
 import 'package:tayseer/my_import.dart';
 
 class AddPostView extends StatelessWidget {
-  const AddPostView({
-    super.key,
-    this.addPostEnum = AddPostEnum.post,
-    this.post,
-    this.isEdit = false,
-  });
-  final AddPostEnum? addPostEnum;
+  const AddPostView({super.key, this.post, this.isEdit = false});
   final PostModel? post;
   final bool isEdit;
   @override
   Widget build(BuildContext context) {
+    log(
+      ">>>>>>>>>>>>>>>>>>>>>>>>>post in add post view is : ${post?.content} ,  isEdit : $isEdit",
+    );
     return Scaffold(body: AddPostBody());
   }
 }
