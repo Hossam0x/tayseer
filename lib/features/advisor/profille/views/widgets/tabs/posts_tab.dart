8823a489054b326888b43797c4d4304ec9da36d6
Ahmed <ahmedlshennawy10@gmail.com>
2026-03-07 14:23:14 +0200

    Make block to any user from posts in profiles and saved posts

--- lib/features/advisor/profille/views/widgets/tabs/posts_tab.dart
+++ lib/features/advisor/profille/views/widgets/tabs/posts_tab.dart
@@ -117,11 +117,11 @@ class PostsTab extends StatelessWidget {
     switch (state.shareActionState) {
       case CubitStates.success:
         state.isShareAdded == true
-            ? AppToast.success(context, message ?? 'تمت المشاركة بنجاح')
-            : AppToast.info(context, message ?? 'تم إلغاء المشاركة');
+            ? AppToast.success(context, message ?? context.tr('shared_success'))
+            : AppToast.info(context, message ?? context.tr('unshared_success'));
         break;
       case CubitStates.failure:
-        AppToast.error(context, message ?? 'حدث خطأ أثناء المشاركة');
+        AppToast.error(context, message ?? context.tr('shared_error'));
         break;
       default:
         break;
@@ -132,10 +132,10 @@ class PostsTab extends StatelessWidget {
     final message = state.saveMessage;
     switch (state.saveActionState) {
       case CubitStates.success:
-        AppToast.success(context, message ?? 'تمت العملية بنجاح');
+        AppToast.success(context, message ?? context.tr('operation_success'));
         break;
       case CubitStates.failure:
-        AppToast.error(context, message ?? 'حدث خطأ أثناء الحفظ');
+        AppToast.error(context, message ?? context.tr('save_error'));
         break;
       default:
         break;
@@ -146,10 +146,10 @@ class PostsTab extends StatelessWidget {
     final message = state.deletePostMessage;
     switch (state.deletePostActionState) {
       case CubitStates.success:
-        AppToast.success(context, message ?? 'تم الحذف بنجاح');
+        AppToast.success(context, message ?? context.tr('delete_success'));
         break;
       case CubitStates.failure:
-        AppToast.error(context, message ?? 'حدث خطأ أثناء الحذف');
+        AppToast.error(context, message ?? context.tr('delete_error'));
         break;
       default:
         break;
@@ -160,10 +160,10 @@ class PostsTab extends StatelessWidget {
     final message = state.archivePostMessage;
     switch (state.archivePostActionState) {
       case CubitStates.success:
-        AppToast.success(context, message ?? 'تمت الأرشفة بنجاح');
+        AppToast.success(context, message ?? context.tr('archive_success'));
         break;
       case CubitStates.failure:
-        AppToast.error(context, message ?? 'حدث خطأ أثناء الأرشفة');
+        AppToast.error(context, message ?? context.tr('archive_error'));
         break;
       default:
         break;
@@ -177,13 +177,16 @@ class PostsTab extends StatelessWidget {
         break;
       case CubitStates.success:
         CustomloadingApp.hide(context);
-        AppToast.success(context, state.blockUserMessage ?? 'تم الحظر بنجاح');
+        AppToast.success(
+          context,
+          state.blockUserMessage ?? context.tr('blocked_successfully'),
+        );
         break;
       case CubitStates.failure:
         CustomloadingApp.hide(context);
         AppToast.error(
           context,
-          state.blockUserMessage ?? 'حدث خطأ أثناء الحظر',
+          state.blockUserMessage ?? context.tr('failed_to_block'),
         );
         break;
       default:
@@ -372,7 +375,7 @@ class _PostItemState extends State<_PostItem> {
   void _archivePost(String postId) =>
       widget.profileCubit.archivePost(postId: postId);
 
-  void _blockUser(String userId, String postId) =>
+  void _blockUser(String postId, String userId) =>
       widget.profileCubit.blockUser(visiblePostId: postId, advisorId: userId);
 
   void _hidePost(String postId) =>
