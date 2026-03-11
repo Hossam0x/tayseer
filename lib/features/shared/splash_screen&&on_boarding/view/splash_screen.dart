bc827b7f89b4ff27c6e177849e0f38267d8b7462
khaledaboelhamd <powershop9640@gmail.com>
2026-03-11 13:10:41 +0200

    nav to registration if user doesn't have token

--- lib/features/shared/splash_screen&&on_boarding/view/splash_screen.dart
+++ lib/features/shared/splash_screen&&on_boarding/view/splash_screen.dart
@@ -50,11 +50,10 @@ class _SplashScreenState extends State<SplashScreen>
           kCurrentUserData?.isNew == false
               ? context.pushReplacementNamed(AppRouter.kUserLayoutView)
               : context.pushReplacementNamed(AppRouter.kRegisrationView);
-          
         }
       } else {
         navigatorKey.currentState?.pushReplacementNamed(
-          AppRouter.kUserLayoutView,
+          AppRouter.kRegisrationView,
         );
       }
 
