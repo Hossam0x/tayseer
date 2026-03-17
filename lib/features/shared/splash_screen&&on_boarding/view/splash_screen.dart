074343adbecd1b6ec2ed9cca2b86e1ba057d6aab
khaledaboelhamd <powershop9640@gmail.com>
2026-03-17 16:41:50 +0200

    Adjust splash screen delay from 5 seconds to 4800 milliseconds for improved user experience.

--- lib/features/shared/splash_screen&&on_boarding/view/splash_screen.dart
+++ lib/features/shared/splash_screen&&on_boarding/view/splash_screen.dart
@@ -53,7 +53,7 @@ class _SplashScreenState extends State<SplashScreen>
   }
 
   Future<void> _navigateBasedOnToken() async {
-    await Future.delayed(const Duration(seconds: 5));
+    await Future.delayed(const Duration(milliseconds: 4800));
     if (!mounted) return;
 
     String? token = CachNetwork.getStringData(key: ktoken);
