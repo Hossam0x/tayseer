dc1815e2a1e6211e2a07e6e922974c128aafbea9
khaledaboelhamd <powershop9640@gmail.com>
2026-03-26 14:16:07 +0200

    fix: update AppImage dimensions to use MediaQuery for better responsiveness

--- lib/features/advisor/layout/views/widgets/guest_lock_widget.dart
+++ lib/features/advisor/layout/views/widgets/guest_lock_widget.dart
@@ -33,8 +33,8 @@ class GuestLockWidget extends StatelessWidget {
           if (showBackgroundImage)
             AppImage(
               AssetsData.homeBarBackgroundImage,
-              width: double.infinity,
-              height: context.height * 0.05,
+              width: MediaQuery.sizeOf(context).width,
+              height: MediaQuery.sizeOf(context).height * 0.05,
               fit: BoxFit.cover,
             ),
 
