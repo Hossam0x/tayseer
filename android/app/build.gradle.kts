a8e5aa9602b4882de3122096cbadae4a62a2b39b
Ahmed <ahmedlshennawy10@gmail.com>
2026-04-20 13:25:02 +0200

    Change version code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.athr.tayser"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
+        versionCode = 9
         versionName = "1.4.5"
         multiDexEnabled = true
     }
